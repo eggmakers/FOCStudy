ed99d380e71ae96730423a093e2d82d969ece2f7
eggmaker <1402461065@qq.com>
2023-11-26 21:06:30 +0800

    openloop control

--- Openloop_control/Core/Src/main.c
+++ Openloop_control/Core/Src/main.c
@@ -1,20 +1,20 @@
 /* USER CODE BEGIN Header */
 /**
-  ******************************************************************************
-  * @file           : main.c
-  * @brief          : Main program body
-  ******************************************************************************
-  * @attention
-  *
-  * Copyright (c) 2023 STMicroelectronics.
-  * All rights reserved.
-  *
-  * This software is licensed under terms that can be found in the LICENSE file
-  * in the root directory of this software component.
-  * If no LICENSE file comes with this software, it is provided AS-IS.
-  *
-  ******************************************************************************
-  */
+ ******************************************************************************
+ * @file           : main.c
+ * @brief          : Main program body
+ ******************************************************************************
+ * @attention
+ *
+ * Copyright (c) 2023 STMicroelectronics.
+ * All rights reserved.
+ *
+ * This software is licensed under terms that can be found in the LICENSE file
+ * in the root directory of this software component.
+ * If no LICENSE file comes with this software, it is provided AS-IS.
+ *
+ ******************************************************************************
+ */
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
@@ -62,6 +62,23 @@ void SystemClock_Config(void);
 
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
+uint32_t micros(void)
+{
+  static uint32_t usTicks = 0;
+  static uint32_t lastTickCount = 0;
+  uint32_t tickCount = HAL_GetTick();
+
+  if (tickCount != lastTickCount)
+  {
+    // SysTick is a 24-bit counter, and the frequency is SysClock / 1000 (because HAL_GetTick is in milliseconds)
+    usTicks += (tickCount - lastTickCount) * (HAL_RCC_GetSysClockFreq() / 1000);
+    lastTickCount = tickCount;
+  }
+
+  // Return the current microsecond count
+  return usTicks + ((SysTick->LOAD - SysTick->VAL) * 1000000) / SysTick->LOAD;
+}
+
 float _electricalAngle(float shaft_angle, int pole_pairs)
 {
   return (shaft_angle * pole_pairs);
@@ -72,10 +89,6 @@ float _normalizeAngle(float angle)
 {
   float a = fmod(angle, 2 * PI); // 取余运算可以用于归一化，列出特殊值例子算便知
   return a >= 0 ? a : (a + 2 * PI);
-  // 三目运算符。格式：condition ? expr1 : expr2
-  // 其中，condition 是要求值的条件表达式，如果条件成立，则返回 expr1 的值，否则返回 expr2 的值。可以将三目运算符视为 if-else 语句的简化形式。
-  // fmod 函数的余数的符号与除数相同。因此，当 angle 的值为负数时，余数的符号将与 _2PI 的符号相反。也就是说，如果 angle 的值小于 0 且 _2PI 的值为正数，则 fmod(angle, _2PI) 的余数将为负数。
-  // 例如，当 angle 的值为 -PI/2，_2PI 的值为 2PI 时，fmod(angle, _2PI) 将返回一个负数。在这种情况下，可以通过将负数的余数加上 _2PI 来将角度归一化到 [0, 2PI] 的范围内，以确保角度的值始终为正数。
 }
 
 void setPWM(float Ua, float Ub, float Uc)
@@ -90,9 +103,14 @@ void setPWM(float Ua, float Ub, float Uc)
   dc_b = _constrain(Ub / voltage_power_supply, 0.f, 1.f);
   dc_c = _constrain(Uc / voltage_power_supply, 0.f, 1.f);
 
-  __HAL_TIM_SetCompare(&htim2, TIM_CHANNEL_1, dc_a * 1280);
-  __HAL_TIM_SetCompare(&htim2, TIM_CHANNEL_1, dc_b * 1280);
-  __HAL_TIM_SetCompare(&htim2, TIM_CHANNEL_1, dc_c * 1280);
+  char buffer[12];
+  sprintf(buffer, "%f", dc_b);
+  HAL_UART_Transmit(&huart1, "\r\n", strlen("\r\n"), 0xFFFF);
+  HAL_UART_Transmit(&huart1, (uint8_t *)buffer, strlen(buffer), 0xFFFF);
+
+  __HAL_TIM_SetCompare(&htim2, TIM_CHANNEL_1, dc_a * 2399);
+  __HAL_TIM_SetCompare(&htim2, TIM_CHANNEL_1, dc_b * 2399);
+  __HAL_TIM_SetCompare(&htim2, TIM_CHANNEL_1, dc_c * 2399);
 }
 
 void setPhaseVoltage(float Uq, float Ud, float angle_el)
@@ -112,22 +130,16 @@ void setPhaseVoltage(float Uq, float Ud, float angle_el)
 // 开环速度函数
 float velocityOpenloop(float target_velocity)
 {
-  unsigned long now_us = HAL_GetTick(); // 获取从开启芯片以来的微秒数，它的精度是 4 微秒。 micros() 返回的是一个无符号长整型（unsigned long）的值
+  uint32_t now_us = micros(); // 获取从开启芯片以来的微秒数，它的精度是 4 微秒。 micros() 返回的是一个无符号长整型（unsigned long）的值
 
   // 计算当前每个Loop的运行时间间隔
   float Ts = (now_us - open_loop_timestamp) * 1e-6f;
 
-  // 由于 micros() 函数返回的时间戳会在大约 70 分钟之后重新开始计数，在由70分钟跳变到0时，TS会出现异常，因此需要进行修正。如果时间间隔小于等于零或大于 0.5 秒，则将其设置为一个较小的默认值，即 1e-3f
   if (Ts <= 0 || Ts > 0.5f)
     Ts = 1e-3f;
 
-  // 通过乘以时间间隔和目标速度来计算需要转动的机械角度，存储在 shaft_angle 变量中。在此之前，还需要对轴角度进行归一化，以确保其值在 0 到 2π 之间。
   shaft_angle = _normalizeAngle(shaft_angle + target_velocity * Ts);
-  // 以目标速度为 10 rad/s 为例，如果时间间隔是 1 秒，则在每个循环中需要增加 10 * 1 = 10 弧度的角度变化量，才能使电机转动到目标速度。
-  // 如果时间间隔是 0.1 秒，那么在每个循环中需要增加的角度变化量就是 10 * 0.1 = 1 弧度，才能实现相同的目标速度。因此，电机轴的转动角度取决于目标速度和时间间隔的乘积。
 
-  // 使用早前设置的voltage_power_supply的1/3作为Uq值，这个值会直接影响输出力矩
-  // 最大只能设置为Uq = voltage_power_supply/2，否则ua,ub,uc会超出供电电压限幅
   float Uq = voltage_power_supply / 3;
 
   setPhaseVoltage(Uq, 0, _electricalAngle(shaft_angle, 7));
@@ -139,9 +151,9 @@ float velocityOpenloop(float target_velocity)
 /* USER CODE END 0 */
 
 /**
-  * @brief  The application entry point.
-  * @retval int
-  */
+ * @brief  The application entry point.
+ * @retval int
+ */
 int main(void)
 {
   /* USER CODE BEGIN 1 */
@@ -183,23 +195,23 @@ int main(void)
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
-		velocityOpenloop(5);
+    velocityOpenloop(25);
   }
   /* USER CODE END 3 */
 }
 
 /**
-  * @brief System Clock Configuration
-  * @retval None
-  */
+ * @brief System Clock Configuration
+ * @retval None
+ */
 void SystemClock_Config(void)
 {
   RCC_OscInitTypeDef RCC_OscInitStruct = {0};
   RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
 
   /** Initializes the RCC Oscillators according to the specified parameters
-  * in the RCC_OscInitTypeDef structure.
-  */
+   * in the RCC_OscInitTypeDef structure.
+   */
   RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
   RCC_OscInitStruct.HSEState = RCC_HSE_ON;
   RCC_OscInitStruct.HSEPredivValue = RCC_HSE_PREDIV_DIV1;
@@ -213,9 +225,8 @@ void SystemClock_Config(void)
   }
 
   /** Initializes the CPU, AHB and APB buses clocks
-  */
-  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
-                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+   */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
   RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
   RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
   RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
@@ -232,9 +243,9 @@ void SystemClock_Config(void)
 /* USER CODE END 4 */
 
 /**
-  * @brief  This function is executed in case of error occurrence.
-  * @retval None
-  */
+ * @brief  This function is executed in case of error occurrence.
+ * @retval None
+ */
 void Error_Handler(void)
 {
   /* USER CODE BEGIN Error_Handler_Debug */
@@ -246,14 +257,14 @@ void Error_Handler(void)
   /* USER CODE END Error_Handler_Debug */
 }
 
-#ifdef  USE_FULL_ASSERT
+#ifdef USE_FULL_ASSERT
 /**
-  * @brief  Reports the name of the source file and the source line number
-  *         where the assert_param error has occurred.
-  * @param  file: pointer to the source file name
-  * @param  line: assert_param error line source number
-  * @retval None
-  */
+ * @brief  Reports the name of the source file and the source line number
+ *         where the assert_param error has occurred.
+ * @param  file: pointer to the source file name
+ * @param  line: assert_param error line source number
+ * @retval None
+ */
 void assert_failed(uint8_t *file, uint32_t line)
 {
   /* USER CODE BEGIN 6 */
